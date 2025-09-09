288687f18bd975c6264eb0caf4298ca8747ef1ad
tanel sarik <tanel.sarik@tptlive.ee>
2025-09-09 13:12:33 +0300

    Add button comonent

---
+++ src/components/Btn.vue
@@ -0,0 +1,49 @@
+<script setup>
+import { computed } from 'vue';
+ let props = defineProps({
+  color: {
+    type: String,
+    required: false,
+    validator(value) {
+      return [
+        'primary',
+        'link',
+        'info',
+        'success',
+        'warning',
+        'danger',
+        'white',
+        'light',
+        'dark',
+        'text',
+        'ghost',
+      ].includes(value);
+    }
+  },
+  light: {
+    type: Boolean,
+    required: false,
+  }
+});
+
+let classNames= computed(() =>{
+   let names = [];
+   if(props.color){
+    names.push('is-'+ props.color);
+   }
+   if(props.light){
+    names.push('is-light');
+   }
+  
+    return names.join(' ');
+})
+
+
+</script>
+
+<template>
+    
+  <button class="button" :class="classNames">
+    <slot></slot>
+  </button>
+</template>
