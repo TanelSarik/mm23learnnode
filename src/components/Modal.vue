d7c88275248aa9bedf2d25ee4e4ea288e818e5bb
tanel sarik <tanel.sarik@tptlive.ee>
2025-09-09 12:37:32 +0300

    add modal component and example

---
+++ src/components/Modal.vue
@@ -0,0 +1,19 @@
+<script setup>
+const props = defineProps(['active', 'url']);
+const emit = defineEmits(['close']);
+</script>
+
+<template>
+  <div class="modal" :class="{ 'is-active': props.active }">
+    <div class="modal-background" @click="emit('close')"></div>
+    <div class="modal-content">
+        <slot>
+            <p class="image is-4by3">
+        <img :src="props.url" alt="">
+      </p> 
+        </slot>
+        
+    </div>
+    <button class="modal-close is-large" aria-label="close" @click="emit('close')"></button>
+  </div>
+</template>
