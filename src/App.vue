97c532ca4ff7f706cf14b506bfa711644a728257
tanel sarik <tanel.sarik@tptlive.ee>
2025-09-09 10:13:07 +0300

    V-model example

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <script setup>
-  import { ref } from 'vue';
+import { ref } from "vue";
 
-  let message = ref('Hello vue');
-  setTimeout(() => {
-    message.value = 'something else';
-  }, 10000);
+let message = ref("Hello vue");
+
+
 </script>
 
 <template>
-  <div id="app">
-    <h1>{{ message }}</h1>
+  <div class="container mt-2">
+    <input type="input" v-model="message">
+    <div class="content">
+    <h1>{{ message.split('').reverse().join('')  }}</h1>
+    </div>
   </div>
 </template>
