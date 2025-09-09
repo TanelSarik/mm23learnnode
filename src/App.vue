450eb83f715acf5f4eee1065f8103b95d0127886
tanel sarik <tanel.sarik@tptlive.ee>
2025-09-09 13:46:10 +0300

    Move Buttons code to separate page file

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <script setup>
-import Btn from './components/Btn.vue';
 
 </script>
 <template>
-    <div class="container mt-2">
-        <btn color="primary" dark size="large" outlined  rounded state="loading"> Button is cool</btn>
-        <button class="button is-primary is-outlined is-inverted is-loading">Test button</button>
-    </div>
+    
 </template>
---
+++ src/pages/Buttons.vue
@@ -0,0 +1,10 @@
+<script setup>
+import Btn from './components/Btn.vue';
+
+</script>
+<template>
+    <div class="container mt-2">
+        <btn color="primary" dark size="large" outlined  rounded state="loading"> Button is cool</btn>
+        <button class="button is-primary is-outlined is-inverted is-loading">Test button</button>
+    </div>
+</template>
