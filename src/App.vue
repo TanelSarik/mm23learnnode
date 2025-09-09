341f88f805572dd68aa1a8751e82a7cd699fdd57
tanel sarik <tanel.sarik@tptlive.ee>
2025-09-09 13:44:05 +0300

    finish button component

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ import Btn from './components/Btn.vue';
 </script>
 <template>
     <div class="container mt-2">
-        <btn color="primary" light> Button is cool</btn>
+        <btn color="primary" dark size="large" outlined  rounded state="loading"> Button is cool</btn>
+        <button class="button is-primary is-outlined is-inverted is-loading">Test button</button>
     </div>
 </template>
--- src/components/Btn.vue
+++ src/components/Btn.vue
@@ -1,49 +1,106 @@
+
 <script setup>
 import { computed } from 'vue';
- let props = defineProps({
+
+const props = defineProps({
   color: {
     type: String,
     required: false,
     validator(value) {
       return [
-        'primary',
-        'link',
-        'info',
-        'success',
-        'warning',
-        'danger',
-        'white',
-        'light',
-        'dark',
-        'text',
-        'ghost',
+        'primary', 'link', 'info', 'success', 'warning',
+        'danger', 'white', 'light', 'dark', 'text', 'ghost'
       ].includes(value);
     }
   },
   light: {
     type: Boolean,
     required: false,
-  }
+  },
+  size: {
+    type: String,
+    required: false,
+    validator(value) {
+      return ['small', 'normal', 'medium', 'large'].includes(value);
+    }
+  },
+  resposive: {
+    type: Boolean,
+    required: false,
+  },
+  fullwidth: {
+    type: Boolean,
+    required: false,
+  },
+  outlined: {
+    type: Boolean,
+    required: false,
+  },
+  inverted: {
+    type: Boolean,
+    required: false,
+  },
+  rounded: {
+    type: Boolean,
+    required: false,
+  },
+  state: {
+    type: String,
+    required: false,
+    validator(value, props) {
+      return ['hover', 'focus', 'active', 'loading', 'static'].includes(value);
+    }
+  },
+  disabled: {
+    type: Boolean,
+    required: false,
+  },
+ 
 });
 
-let classNames= computed(() =>{
-   let names = [];
-   if(props.color){
-    names.push('is-'+ props.color);
-   }
-   if(props.light){
+const classNames = computed(() => {
+  let names = [];
+
+  if (props.color) {
+    names.push('is-' + props.color);
+  }
+
+  if (props.light) {
     names.push('is-light');
-   }
-  
-    return names.join(' ');
-})
+  }
+
+  if (props.size) {
+    names.push('is-' + props.size);
+  }
+  if (props.resposive) {
+    names.push('is-responsive');
+  }
+  if (props.resposive) {
+    names.push('is-fullwidth');
+  }
+  if (props.outlined) {
+    names.push('is-outlined');
+  }
+  if (props.inverted) {
+    names.push('is-inverted');
+  }
+  if (props.state) {
+    names.push('is-' + props.state);
+  }
+  if (props.rounded) {
+    names.push('is-rounded');
+  }
+   if (props.disabled) {
+    names.push('is-disabled');
+  }
 
 
+  return names.join(' ');
+});
 </script>
 
 <template>
-    
-  <button class="button" :class="classNames">
+  <button :class="['button', classNames]">
     <slot></slot>
   </button>
 </template>
