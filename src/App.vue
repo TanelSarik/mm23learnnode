31a05de5f069c6dacf7a6659b17f7022362b84be
tanel sarik <tanel.sarik@tptlive.ee>
2025-09-09 11:52:03 +0300

    Move components to folders

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,7 @@
 <script setup>
-import { ref, computed } from "vue";
-import ItemList from './ItemList.vue';
 
-let newItem = ref("Hello vue");
-
-let items = ref([
-  { text: 'piim', done: true },
-  { text: 'viin', done: false },
-  { text: 'sai', done: true },
-  { text: 'leib', done: false },
-  { text: 'olu', done: true },
-  { text: 'banaan', done: false },
-]);
-
-function add() {
-  if (newItem.value.trim() !== "") {
-    items.value.push({ text: newItem.value, done: false });
-    newItem.value = '';
-  }
-}
-
-let doneItems = computed(() => items.value.filter(item => item.done));
-let toDoItems = computed(() => items.value.filter(item => !item.done));
 </script>
-
 <template>
-  <div class="container mt-2">
-    <div class="field has-addons">
-      <div class="control is-expanded">
-        <input class="input" type="text" v-model="newItem" @keydown.enter="add" />
-      </div>
-      <div class="control">
-        <button class="button is-info" @click="add">Add</button>
-      </div>
-    </div>
 
-    <div class="content">
-      <h2>Kõik</h2>
-      <ItemList :items="items" title="All Items" >
 
-      <h2>Tehtud</h2>
-      <ItemList :items="doneItems" />
-
-      <h2>Tegemata</h2>
-      <ItemList :items="toDoItems" />
-    </div>
-  </div>
 </template>
