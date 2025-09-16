c4235f740a6e727507243f23f4a775b24af9d13d
tanel sarik <tanel.sarik@tptlive.ee>
2025-09-16 11:05:22 +0300

    Add prevent default example

--- src/pages/Buttons.vue
+++ src/pages/Buttons.vue
@@ -6,5 +6,6 @@ import Btn from '../components/Btn.vue';
     <div class="container mt-2">
         <btn color="primary" dark size="large" outlined  rounded state="loading"> Button is cool</btn>
         <button class="button is-primary is-outlined is-inverted is-loading">Test button</button>
+        <a href="https://google.com" @click.prevent="console.log('hello')">Google</a>
     </div>
 </template>
