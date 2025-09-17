d0a8abf34252a4a746dfd86ee2fc0c4012c41e65
tanel sarik <tanel.sarik@tptlive.ee>
2025-09-17 16:27:33 +0300

    add go to home nupp, polygon ja cirlce

--- src/pages/maps.vue
+++ src/pages/maps.vue
@@ -1,19 +1,67 @@
 <script setup>
-import L from 'leaflet';
-import 'leaflet/dist/leaflet.css';
-import { onMounted } from 'vue';
+import L from 'leaflet'
+import 'leaflet/dist/leaflet.css'
+import { onMounted } from 'vue'
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png'
+import markerIcon from 'leaflet/dist/images/marker-icon.png'
+import markerShadow from 'leaflet/dist/images/marker-shadow.png'
 
-onMounted(()=> {
-     var map = L.map('map').setView([51.505, -0.09], 13);
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(map);
-}); 
+delete L.Icon.Default.prototype._getIconUrl
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+})
+
+let map = null
+
+onMounted(() => {
+  map = L.map('map').setView([59.4032, 24.6573], 16)
+
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }).addTo(map)
+
+ 
+  const markerCoords = [59.4032, 24.6573]
+  L.marker(markerCoords).addTo(map).bindPopup("Minu koht")
+
+  
+  L.circle(markerCoords, {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.3,
+    radius: 100, 
+  }).addTo(map).bindPopup("100m ring")
+
+
+  L.polygon([
+    [59.4036, 24.6575],
+    [59.4030, 24.6580],
+    [59.4032, 24.6565],
+  ], {
+    color: 'blue',
+    fillColor: 'blue',
+    fillOpacity: 0.2,
+  }).addTo(map).bindPopup("Kolmnurkne ala")
+})
+
+function goTo(coords, zoom) {
+  map.flyTo(coords, zoom)
+}
 </script>
+
 <template>
-     <div id="map"></div>
+  <button class="button is-primary" @click="goTo([59.4032, 24.6573], 17)">Go to Home</button>
+  <div id="map" class="map-container"></div>
 </template>
+
 <style scoped>
-#map { height: 180px; }
+.map-container {
+  height: 90vh;
+  width: 100%;
+}
 </style>
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,98 +1,76 @@
+// webpack.config.js
 import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
-import autoprefixer from 'autoprefixer';
 import { fileURLToPath } from 'url';
 import { VueLoaderPlugin } from 'vue-loader';
+import autoprefixer from 'autoprefixer';
 import webpack from 'webpack';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-
-
-export default  {
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean : true,
+export default {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    assetModuleFilename: '[file]',
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
     },
-    devServer: {
-        static: {
-            directory: path.join(__dirname, 'public'),
-        },
-        compress: true,
-        port: 9000,
-        historyApiFallback: true,
-    },  
-     module: {
-        rules: [
+    compress: true,
+    port: 9000,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+      },
+      {
+        test: /\.css$/i,
+        use: ['vue-style-loader', 'css-loader'],
+      },
+      {
+        test: /\.scss$/i,
+        use: [
+          'vue-style-loader',
+          'css-loader',
           {
-            test: /\.css$/i,
-            use: [ "style-loader" , "css-loader"],
-          },
-          {
-            test: /\.css$/i,
-            use: ["style-loader", "css-loader"],
-          },
-          {
-            test: /\.(scss)$/,
-            use: [
-              {
-                // Adds CSS to the DOM by injecting a `<style>` tag
-                loader: 'style-loader'
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [autoprefixer()],
               },
-              {
-                // Interprets `@import` and `url()` like `import/require()` and will resolve them
-                loader: 'css-loader'
-              },
-              {
-                // Loader for webpack to process CSS with PostCSS
-                loader: 'postcss-loader',
-                options: {
-                  postcssOptions: {
-                    plugins: [
-                       autoprefixer
-                    ]
-                  }
-                }
-              },
-              {
-                // Loads a SASS/SCSS file and compiles it to CSS
-                loader: 'sass-loader',
-                options: {
-                  sassOptions: {
-                    quietDeps: true
-                  }
-                }
-              }
-            ]
+            },
           },
-          {   test:/\.njk$/,
-              use: [
-                {
-                  loader: 'simple-nunjucks-loader',
-                  options: {}
-                },
-              ],
-            }, 
-            {
-              test:/\.vue$/,
-              loader: 'vue-loader'
-            }        
-            ],
-
-        },
-      
-    plugins: [ 
-        new HtmlWebpackPlugin({
-           template: './src/index.html',
-        }),
-        new VueLoaderPlugin(),
-       new webpack.DefinePlugin({
-  __VUE_OPTIONS_API__: JSON.stringify(true),
-  __VUE_PROD_DEVTOOLS__: JSON.stringify(false),
-  __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: JSON.stringify(false)
-})
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|woff2?|eot|ttf|otf)$/,
+        type: 'asset/resource',
+      },
     ],
-} ;  
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new VueLoaderPlugin(),
+    new webpack.DefinePlugin({
+      __VUE_OPTIONS_API__: true,
+      __VUE_PROD_DEVTOOLS__: false,
+    }),
+  ],
+  resolve: {
+    extensions: ['.js', '.vue'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+};
